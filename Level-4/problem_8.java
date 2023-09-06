e9b136d775f965a892a34603e7bbac14db4585ea
sami5671 <samialam5671@gmail.com>
2023-09-06 19:58:06 +0600

    update

--- Level-4/problem_8.java
+++ Level-4/problem_8.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 public class problem_8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter three numbers");
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
         int num3 = sc.nextInt();
 
-        boolean all_equal = (num1 == num2 && num2 == num3);
-        boolean two_equal = (num1 == num2) || (num2 == num3) || (num1 == num3);
-
-        System.out.println(all_equal);
-        System.out.println(two_equal);
-            
-        
+        if (num1 == num2 && num2 == num3 && num1 == num3) {
+            System.out.println("all are equal");
+        }else if(num1 == num2 || num2 == num3 || num1 == num3){
+             System.out.println("two are equal");
+        }
+        else{
+             System.out.println("not all nor two are equal");
+        }  
     }
 }
